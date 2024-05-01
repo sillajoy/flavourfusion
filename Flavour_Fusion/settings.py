8ad33ea0b67287a8a9264254bba794b9bbfa4b41
Silla Joy <sillajoy984@gmail.com>
2024-05-01 09:44:19 +0530

    db

--- Flavour_Fusion/settings.py
+++ Flavour_Fusion/settings.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'Flavour_Fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'flavour_fusion',
-		'USER': 'root',
-		'PASSWORD': 'silla',
-		'HOST':'127.0.0.1',
-		'PORT':'3306',
-	}
-}
 # DATABASES = {
 # 	'default': {
 # 		'ENGINE': 'django.db.backends.mysql',
-# 		'NAME': 'demodb',
+# 		'NAME': 'flavour_fusion',
 # 		'USER': 'root',
-# 		'PASSWORD': 'sillajoy',
-# 		'HOST':'database-1.cnosgue6od5c.ap-southeast-2.rds.amazonaws.com',
+# 		'PASSWORD': 'silla',
+# 		'HOST':'127.0.0.1',
 # 		'PORT':'3306',
 # 	}
 # }
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'demodb',
+		'USER': 'root',
+		'PASSWORD': 'sillajoy',
+		'HOST':'database-1.cnosgue6od5c.ap-southeast-2.rds.amazonaws.com',
+		'PORT':'3306',
+	}
+}
 
 
 # Password validation
